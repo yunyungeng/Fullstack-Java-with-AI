457afd1ec40a117c7b59b1ec3a56ab42f8e59762
CrystalystX19 <93540071+CrystalystX19@users.noreply.github.com>
2026-06-27 09:58:03 +0800

    Completed Day 4 Exercise 01 and uploaded a screenshot

---
+++ js/student-object.js
@@ -0,0 +1,12 @@
+const student = {
+    studentId: "S001",
+    studentName: "Ignacio de Paul",
+    email: "ignacio.de.paul@example.com",
+    status: "Active"
+}
+
+console.log("=== Student Details ===");
+console.log("Student ID:", student.studentId);
+console.log("Student Name:", student.studentName);
+console.log("Email:", student.email);
+console.log("Status:", student.status);
